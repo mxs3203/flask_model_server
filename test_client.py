7b00995bf2773d0aaa24af2c892d20ca7e3df58e
mxs3203 <mxs3203@g.rit.edu>
2020-10-14 00:41:57 +0200

    using no ip and port forawrd

--- test_client.py
+++ test_client.py
@@ -3,19 +3,19 @@ import json
 import requests
 from PIL import Image
 print("Wrong pass example")
-url = 'http://localhost:5000/api/login'
+url = 'http://overunderapi.ddns.net:5655/api/login'
 r = requests.post(url,json={'username': 'filipos', 'password':'krivipass'})
 print(r.status_code)
 
 
 print("Correct login example")
-url = 'http://localhost:5000/api/login'
+url = 'http://overunderapi.ddns.net:5655/api/login'
 r = requests.post(url,json={'username': 'filipos', 'password':'Krastavac56'})
 print(json.loads(r.content.decode("utf-8")))
 token = json.loads(r.content.decode("utf-8"))['token']
 
 print("Send Xray Img example")
-url = 'http://localhost:5000/models/xray'
+url = 'http://overunderapi.ddns.net:5655/models/xray'
 my_img = {'image': open('test1.jpg', 'rb')}
 headers = {'token': token}
 headers = headers
@@ -27,7 +27,7 @@ image.save("1.png")
 stream.close()
 #
 #
-url = 'http://localhost:5000/models/coccidia'
+url = 'http://overunderapi.ddns.net:5655/models/coccidia'
 my_img = {'image': open('test2.jpg', 'rb')}
 r = requests.post(url, files=my_img)
 print(r.status_code)
@@ -36,7 +36,7 @@ image = Image.open(stream).convert("RGBA")
 image.save("2.png")
 stream.close()
 
-url = 'http://localhost:5000/models/neutrophil'
+url = 'http://overunderapi.ddns.net:5655/models/neutrophil'
 my_img = {'image': open('test3.jpg', 'rb')}
 r = requests.post(url, files=my_img)
 print(r.status_code)
